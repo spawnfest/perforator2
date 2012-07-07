dfb5f03f77243656ed9340c40c4774c68b89182c
Martynas Pumputis <martynas.pumputis@gmail.com>
2012-07-07 06:54:06 +0200

    Update types

--- src/perforator_ci_types.erl
+++ src/perforator_ci_types.erl
@@ -4,12 +4,18 @@
 
 -export_type([
     project_id/0,
+    project_name/0,
     build_id/0,
-    commit_id/0
+    commit_id/0,
+    timestamp/0,
+    polling_strategy/0
 ]).
 
 %% ============================================================================
 
--type project_id() :: binary().
+-type project_id() :: integer().
+-type project_name() :: integer().
 -type build_id() :: integer().
 -type commit_id() :: binary().
+-type timestamp() :: binary().
+-type polling_strategy() :: {time, integer()} | on_demand.
