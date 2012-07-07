1fc29c182d7a8b320018331771a2281af091c44c
Vytautas Jakutis <vytautas@jakut.is>
2012-07-07 21:50:45 +0300

    changing res_runs event spec

--- priv/js/lib/serverEmulation.js
+++ priv/js/lib/serverEmulation.js
@@ -3,22 +3,25 @@ exports.init = function(page, cb) {
         project.id = String(Math.random());
         page.emit('projectAdded', null, project);
     });
-    page.on('req_runs', function(_, m) {
-        page.emit('res_runs', null, [
-            {
-                id : '8888',
-                started : new Date().getTime(),
-                time : 1000,
-                modules : 2,
-                tests : 4
-            }, {
-                id : '8808',
-                started : new Date().getTime(),
-                time : 900,
-                modules : 2,
-                tests : 4
-            }
-        ]);
+    page.on('req_runs', function(_, projectId) {
+        page.emit('res_runs', null, {
+            projectId : projectId,
+            runs : [
+                {
+                    id : '8888',
+                    started : new Date().getTime(),
+                    time : 1000,
+                    modules : 2,
+                    tests : 4
+                }, {
+                    id : '8808',
+                    started : new Date().getTime(),
+                    time : 900,
+                    modules : 2,
+                    tests : 4
+                }
+            ]
+        });
     });
     page.on('req_projects', function(_, m) {
         page.emit('res_projects', null, [ {
