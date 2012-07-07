b40a2ddbc322a2a40e5d3a221862e6935b24951c
Vytautas Jakutis <vytautas@jakut.is>
2012-07-07 07:03:04 +0300

    run view: introducing the concept of modules

--- priv/js/lib/run.js
+++ priv/js/lib/run.js
@@ -1,5 +1,6 @@
 var t = require('./templates');
 var moment = require('moment');
+var bean = require('bean');
 var bonzo = require('bonzo');
 var qwery = require('qwery');
 var w = require('./window');
@@ -7,44 +8,105 @@ var v = require('valentine');
 
 exports.init = function(page, cb) {
     page.handle(/^\/run\/(.+)$/, function() {
-        var tests = [
+        var modules = [
             {
-                name : 'bla_blabla',
-                time : {
-                    previous : 311,
-                    mean : 300,
-                    current : 300,
-                    max : 400,
-                    min : 200
-                }
+                name : 'test_modulea',
+                tests : [
+                    {
+                        name : 'bla_blabla',
+                        series : {
+                            time : {
+                                previous : 311,
+                                mean : 300,
+                                current : 300,
+                                max : 400,
+                                min : 200
+                            }
+                        }
+                    }, {
+                        name : 'what_ever',
+                        series : {
+                            time : {
+                                previous : 250,
+                                mean : 300,
+                                current : 330,
+                                max : 400,
+                                min : 200
+                            }
+                        }
+                    }
+                ]
             }, {
-                name : 'what_ever',
-                time : {
-                    previous : 250,
-                    mean : 300,
-                    current : 330,
-                    max : 400,
-                    min : 200
-                }
+                name : 'test_moduleb',
+                tests : [
+                    {
+                        name : 'foo_bar',
+                        series : {
+                            time : {
+                                previous : 211,
+                                mean : 300,
+                                current : 300,
+                                max : 400,
+                                min : 200
+                            }
+                        }
+                    }, {
+                        name : 'what_now',
+                        series : {
+                            time : {
+                                previous : 350,
+                                mean : 300,
+                                current : 330,
+                                max : 400,
+                                min : 200
+                            }
+                        }
+                    }
+                ]
             }
         ];
+        v.each(modules, function(module) {
+            v.each(module.tests, function(test) {
+                test.id = 'test-' + module.name + '-' + test.name;
+            });
+        });
         page.body.html(t.run.render({
-            tests : tests,
+            modules : modules,
+            series : [
+                {
+                    name : 'Time',
+                    key : 'time'
+                }, {
+                    name : 'Average Memory Utilization',
+                    key : 'memutil'
+                }, {
+                    name : 'Average CPU Load',
+                    key : 'load'
+                }, {
+                    name : 'Average CPU Utilization',
+                    key : 'cpuutil'
+                }
+            ],
             run : {
                 id : 0
             }
         }));
+        var select = bonzo(qwery('select'));
+        bean.add(select[0], 'change', function() {
+            console.log('TODO', 'change series', select.val());
+        });
         var chart = w.nv.models.bulletChart();
-        v.each(tests, function(test) {
-            var id = 'test-' + test.name;
-            w.d3.select('#' + id).datum({
-                "title":test.name,
-                "subtitle":'milliseconds',
-                "ranges":[test.time.min, test.time.mean, test.time.max],
-                "measures":[test.time.current],
-                "markers":[test.time.previous]
-            }).call(chart);
-            bonzo(qwery('#' + id + ' .measure')).css('fill', test.time.current > test.time.previous ? '#f00' : '#0f0');
+        v.each(modules, function(module) {
+            v.each(module.tests, function(test) {
+                w.d3.select('#' + test.id).datum({
+                    "title":test.name,
+                    "subtitle":'milliseconds',
+                    "ranges":[test.series.time.min, test.series.time.mean, test.series.time.max],
+                    "measures":[test.series.time.current],
+                    "markers":[test.series.time.previous]
+                }).call(chart);
+                bonzo(qwery('#' + test.id + ' .measure')).css('fill', test.series.time.current > test.series.time.previous ? '#f00' : '#0f0');
+            });
         });
     });
     cb();
