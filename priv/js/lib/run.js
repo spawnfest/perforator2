4dbc4019f4cb75cfc1a67ac40a65b2246f16830b
Vytautas Jakutis <vytautas@jakut.is>
2012-07-07 22:44:49 +0300

    add and use events "req_run" and "res_run"

--- priv/js/lib/run.js
+++ priv/js/lib/run.js
@@ -9,135 +9,64 @@ var series = require('./series');
 series = series.series;
 
 exports.init = function(page, cb) {
-    page.handle(/^\/run\/(.+)$/, function() {
-        var modules = [
-            {
-                name : 'test_modulea',
-                tests : [
-                    {
-                        name : 'bla_blabla',
-                        series : {
-                            memutil : {
-                                previous : 100,
-                                mean : 90,
-                                current : 110,
-                                max : 200,
-                                min : 50
-                            },
-                            time : {
-                                previous : 311,
-                                current : 300
-                            }
-                        }
-                    }, {
-                        name : 'what_ever',
-                        series : {
-                            memutil : {
-                                previous : 150,
-                                mean : 90,
-                                current : 130,
-                                max : 200,
-                                min : 50
-                            },
-                            time : {
-                                previous : 250,
-                                current : 330
-                            }
-                        }
-                    }
-                ]
-            }, {
-                name : 'test_moduleb',
-                tests : [
-                    {
-                        name : 'foo_bar',
-                        series : {
-                            memutil : {
-                                previous : 100,
-                                mean : 120,
-                                current : 130,
-                                max : 250,
-                                min : 100
-                            },
-                            time : {
-                                previous : 211,
-                                current : 300
-                            }
-                        }
-                    }, {
-                        name : 'what_now',
-                        series : {
-                            memutil : {
-                                previous : 120,
-                                mean : 140,
-                                current : 130,
-                                max : 190,
-                                min : 70
-                            },
-                            time : {
-                                previous : 350,
-                                current : 330
-                            }
-                        }
-                    }
-                ]
-            }
-        ];
-        v.each(modules, function(module) {
-            v.each(module.tests, function(test) {
-                test.id = 'test-' + module.name + '-' + test.name;
-            });
-        });
-        var seriesMap = {};
-        v.each(series, function(series) {
-            seriesMap[series.key] = series;
-        });
-        page.body.html(t.run.render({
-            modules : modules,
-            series : series,
-            run : {
-                id : 0
-            }
-        }));
-        var select = bonzo(qwery('select'));
-        bean.add(select[0], 'change', function() {
-            setSeries(select.val());
-        });
-        var chart = w.nv.models.bulletChart();
-        v.each(modules, function(module) {
-            v.each(module.tests, function(test) {
-                bean.add(qwery('#' + test.id)[0].parentNode.parentNode, 'click', function() {
-                    page.go('/test/' + module.name + '/' + test.name);
-                });
-            });
-        });
-        var currentSeries = null;
-        var setSeries = function(key) {
-            currentSeries = key;
+    page.handle(/^\/run\/(.+)$/, function(from, to, params) {
+        var id = params[0];
+        page.req('run', null, id, function(_, res) {
+            var modules = res.modules;
             v.each(modules, function(module) {
                 v.each(module.tests, function(test) {
-                    bonzo(qwery('#' + test.id)).empty();
-                    var data = test.series[key];
-                    var ranges = [];
-                    ranges.push(typeof data.min === 'undefined' ? 0 : data.min);
-                    if(typeof data.mean !== 'undefined') {
-                        ranges.push(data.mean);
-                    }
-                    ranges.push(typeof data.max === 'undefined' ? Math.max(data.current, data.previous) : data.max);
-                    w.d3.select('#' + test.id).datum({
-                        title: test.name,
-                        subtitle: seriesMap[key].units,
-                        ranges: ranges,
-                        measures: [data.current],
-                        markers: [data.previous]
-                    }).call(chart);
-                    bonzo(qwery('#' + test.id + ' .measure')).css('fill', test.series[key].current > test.series[key].previous ? (seriesMap[key].higherBetter ? '#0f0' : '#f00') : (seriesMap[key].higherBetter ? '#f00' : '#0f0'));
+                    test.id = 'test-' + module.name + '-' + test.name;
                 });
             });
-        };
-        setSeries('time');
-        w.nv.utils.windowResize(function() {
-            setSeries(currentSeries);
+            var seriesMap = {};
+            v.each(series, function(series) {
+                seriesMap[series.key] = series;
+            });
+            page.body.html(t.run.render({
+                modules : modules,
+                series : series,
+                id : id
+            }));
+            var select = bonzo(qwery('select'));
+            bean.add(select[0], 'change', function() {
+                setSeries(select.val());
+            });
+            var chart = w.nv.models.bulletChart();
+            v.each(modules, function(module) {
+                v.each(module.tests, function(test) {
+                    bean.add(qwery('#' + test.id)[0].parentNode.parentNode, 'click', function() {
+                        page.go('/test/' + module.name + '/' + test.name);
+                    });
+                });
+            });
+            var currentSeries = null;
+            var setSeries = function(key) {
+                currentSeries = key;
+                v.each(modules, function(module) {
+                    v.each(module.tests, function(test) {
+                        bonzo(qwery('#' + test.id)).empty();
+                        var data = test.series[key];
+                        var ranges = [];
+                        ranges.push(typeof data.min === 'undefined' ? 0 : data.min);
+                        if(typeof data.mean !== 'undefined') {
+                            ranges.push(data.mean);
+                        }
+                        ranges.push(typeof data.max === 'undefined' ? Math.max(data.current, data.previous) : data.max);
+                        w.d3.select('#' + test.id).datum({
+                            title: test.name,
+                            subtitle: seriesMap[key].units,
+                            ranges: ranges,
+                            measures: [data.current],
+                            markers: [data.previous]
+                        }).call(chart);
+                        bonzo(qwery('#' + test.id + ' .measure')).css('fill', test.series[key].current > test.series[key].previous ? (seriesMap[key].higherBetter ? '#0f0' : '#f00') : (seriesMap[key].higherBetter ? '#f00' : '#0f0'));
+                    });
+                });
+            };
+            setSeries('time');
+            w.nv.utils.windowResize(function() {
+                setSeries(currentSeries);
+            });
         });
     });
     cb();
