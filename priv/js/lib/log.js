4a73c4bbb4a302ba58d779bb777f3d807d0a093e
Vytautas Jakutis <vytautas@jakut.is>
2012-07-07 21:18:06 +0300

    js: introducing serverEmulation, project template, implementing project adding

--- priv/js/lib/log.js
+++ priv/js/lib/log.js
@@ -1,56 +1,91 @@
 var t = require('./templates');
 var v = require('valentine');
 var qwery = require('qwery');
+var step = require('step');
 var bean = require('bean');
 var bonzo = require('bonzo');
 var moment = require('moment');
 
+var insertProject = function(projects, project) {
+    for(var i = 0; i < projects.length; i += 1) {
+        if(projects[i].title > project.title) {
+            projects[i].splice(i, 0, project);
+            return {
+                after : false,
+                project : projects[i + 1]
+            };
+        }
+    }
+    projects.push(project);
+    return {
+        after : projects.length === 1 ? null : true,
+        project : projects[projects.length - 2]
+    };
+};
+
 exports.init = function(page, cb) {
-    page.handle('/', function() {
-        page.go('/project/3ttat');
-    });
-    page.handle(/^\/project\/(.+)$/, function(from, to, params) {
-        var id = params[0];
-        page.body.html(t.log.render({
-            currentId : id,
-            projects : [
-                {
-                    id : '3ttat',
-                    title : 'Project omg #1',
-                    opened : id === '3ttat'
-                },
-                {
-                    id : '3hsdg',
-                    title : 'Project omg #2',
-                    opened : id === '3hsdg'
+    step(function() {
+        page.req('projects', null, null, this);
+    }, function(_, projects) {
+        page.once('projectAdded', function(_, project) {
+            // ABSTRACT THIS SHIT
+            var position = insertProject(projects, project);
+            var html = t.project.render(project);
+            if(position.after === null) {
+                bonzo(qwery('#projects-header')).append(html);
+            } else {
+                if(position.after) {
+                    bonzo(qwery('#project-' + position.project.id)).after(html);
+                } else {
+                    bonzo(qwery('#project-' + position.project.id)).before(html);
                 }
-            ],
-            runs : [
-                {
-                    id : 0,
-                    started : moment(new Date()).fromNow(),
-                    duration : 1000,
-                    durationDelta : 10,
-                    modules : 2,
-                    tests : 4
-                }, {
-                    id : 1,
-                    started : moment(new Date()).fromNow(),
-                    duration : 900,
-                    durationDelta : -100,
-                    modules : 2,
-                    tests : 4
-                }
-            ],
-            project : {
-                title : 'Project omg #1'
             }
-        }));
-        v.each(qwery('tr'), function(row) {
-            bean.add(row, 'click', function() {
-                page.go('/run/' + bonzo(row).data('id'));
+        });
+        page.handle('/', function() {
+            page.go('/project/3ttat');
+        });
+        page.handle(/^\/project\/(.+)$/, function(from, to, params) {
+            step(function() {
+                page.req('runs', null, params[0], this);
+            }, function(_, runs) {
+                var project = null;
+                var runLag = null;
+
+                runs.reverse();
+                v.each(runs, function(run) {
+                    if(runLag === null) {
+                        run.timeDelta = null;
+                    } else {
+                        run.timeDelta = run.time - runLag.time;
+                    }
+                    runLag = run;
+                });
+                runs.reverse();
+
+                v.each(projects, function(p) {
+                    if(p.id === params[0]) {
+                        project = p;
+                        p.opened = true;
+                    } else {
+                        p.opened = false;
+                    }
+                });
+                v.each(runs, function(run) {
+                    run.started = moment(new Date(run.started)).fromNow();
+                });
+                console.log(projects);
+                page.body.html(t.log.render({
+                    projects : projects,
+                    runs : runs,
+                    project : project
+                }, t));
+                v.each(qwery('tr'), function(row) {
+                    bean.add(row, 'click', function() {
+                        page.go('/run/' + bonzo(row).data('id'));
+                    });
+                });
             });
         });
+        cb();
     });
-    cb();
 };
---
+++ priv/js/lib/serverEmulation.js
@@ -0,0 +1,42 @@
+exports.init = function(page, cb) {
+    page.on('addProject', function(_, project) {
+        project.id = String(Math.random());
+        page.emit('projectAdded', null, project);
+    });
+    page.on('req_runs', function(_, m) {
+        page.emit('res_runs', null, [
+            {
+                id : '8888',
+                started : new Date().getTime(),
+                time : 1000,
+                modules : 2,
+                tests : 4
+            }, {
+                id : '8808',
+                started : new Date().getTime(),
+                time : 900,
+                modules : 2,
+                tests : 4
+            }
+        ]);
+    });
+    page.on('req_projects', function(_, m) {
+        page.emit('res_projects', null, [ {
+                id : '3ttat',
+                title : 'Project omg #1',
+                repo : {
+                    type : 'git',
+                    url : 'git@github.com:omg/proj1'
+                }
+            }, {
+                id : '3hsdg',
+                title : 'Project omg #2000',
+                repo : {
+                    type : 'git',
+                    url : 'git@github.com:omg/proj2'
+                }
+            }
+        ]);
+    });
+    cb(null);
+};
