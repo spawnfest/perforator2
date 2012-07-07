b9877c1f16f407684965e0e278f92e340862d318
Vytautas Jakutis <vytautas@jakut.is>
2012-07-07 04:33:15 +0300

    implementing the Home view with hardcoded data

--- priv/js/lib/log.js
+++ priv/js/lib/log.js
@@ -1,9 +1,39 @@
 var t = require('./templates');
+var moment = require('moment');
 
 exports.init = function(page, cb) {
     page.handle('/', function() {
-        page.body.html(t.test.render({
-            name : 'Vytautas'
+        page.body.html(t.log.render({
+            projects : [
+                {
+                    title : 'Project omg #1',
+                    opened : true
+                },
+                {
+                    title : 'Project omg #2',
+                    opened : false
+                }
+            ],
+            runs : [
+                {
+                    id : 0,
+                    started : moment(new Date()).fromNow(),
+                    duration : 1000,
+                    durationDelta : 10,
+                    modules : 2,
+                    tests : 4
+                }, {
+                    id : 1,
+                    started : moment(new Date()).fromNow(),
+                    duration : 900,
+                    durationDelta : -100,
+                    modules : 2,
+                    tests : 4
+                }
+            ],
+            project : {
+                title : 'Project omg #1'
+            }
         }));
     });
     cb();
