b5e334a36edbb81426c02edf6d5cc4cb90269554
Vytautas Jakutis <vytautas@jakut.is>
2012-07-08 03:12:20 +0300

    update

--- priv/js/lib/run.js
+++ priv/js/lib/run.js
@@ -14,7 +14,7 @@ exports.init = function(page, cb) {
             projectId : params[0],
             id : params[1]
         };
-        page.req('run', null, state, function(_, modules) {
+        page.req('run', state, function(_, modules) {
             v.each(modules, function(module) {
                 v.each(module.tests, function(test) {
                     test.id = 'test-' + module.name + '-' + test.name;
