eba262e9d2086b4069cc28b3765bfa6f987d9651
Vytautas Jakutis <vytautas@jakut.is>
2012-07-09 00:16:29 +0300

    js: series: beginning adapt functions

--- priv/js/lib/series.js
+++ priv/js/lib/series.js
@@ -5,28 +5,56 @@ var series = [
         units : 'milliseconds',
         unitsShort : 'ms',
         precision : 0,
-        higherBetter : false
+        higherBetter : false,
+        adapt : function(val) {
+            return {
+                val : val,
+                unitsShort : 'ms',
+                units : 'milliseconds'
+            };
+        }
     }, {
         name : 'Average Memory Utilization',
         key : 'used_memory',
         units : 'bytes',
         unitsShort : 'b',
         precision : 0,
-        higherBetter : false
+        higherBetter : false,
+        adapt : function(val) {
+            return {
+                val : val,
+                unitsShort : 'b',
+                units : 'bytes'
+            };
+        }
     }, {
         name : 'Average CPU Load',
         key : 'cpu_load',
         units : '',
         unitsShort : '',
         precision : 2,
-        higherBetter : false
+        higherBetter : false,
+        adapt : function(val) {
+            return {
+                val : val,
+                unitsShort : '',
+                units : ''
+            };
+        }
     }, {
         name : 'Average CPU Utilization',
         key : 'cpu_util',
         units : 'percent',
         unitsShort : '%',
         precision : 2,
-        higherBetter : false
+        higherBetter : false,
+        adapt : function(val) {
+            return {
+                val : val,
+                unitsShort : '%',
+                units : 'percent'
+            };
+        }
     }
 ];
 exports.series = series;
